e4d702ec93de155bc9bdc84452c2f8bb83b86f2f
abdulmajeed <mjeed.f.b@outlook.sa>
2025-12-01 18:33:42 +0300

    second commit

--- CarRental/bookings/urls.py
+++ CarRental/bookings/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = "name"
+app_name = "bookings"
 urlpatterns = [
     path("booking_success/", views.booking_success, name="booking_success"),
     path("create_booking/", views.create_booking, name="create_booking"),
--- CarRental/bookings/views.py
+++ CarRental/bookings/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
-def booking_success(request : HttpRequest):
-    pass
+def booking_success(request):
+    return render(request, 'booking_success.html')
 
-def create_booking(request : HttpRequest):
-    pass
+def create_booking(request):
+    return render(request, 'create_booking.html')
 
-def reviewer_dashboard(request : HttpRequest):
-    pass
+def reviewer_dashboard(request):
+    return render(request, 'reviewer_dashboard.html')
+
 
